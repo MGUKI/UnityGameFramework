511d7453b3940e20eb0b220debb5e0841d208e3c
MangoKI <347289276@qq.com>
2021-12-15 20:39:30 +0800

    通过委托处理表现逻辑
    1.给mCount添加属性，写入(Set)值时调用委托回调
    2.Start中订阅方法，这个方法用来实现表现逻辑

--- GameFramework/Assets/InteractionAndmanifestation/Scripts/ViewController.cs
+++ GameFramework/Assets/InteractionAndmanifestation/Scripts/ViewController.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,22 +9,53 @@ namespace MyFramework
         private Text number;
         void Start()
         {
+            ViewModel.mOnEvent += OnCountChanged;
+            
             number = transform.Find("Canvas/number").GetComponent<Text>();
-            UpdateView();
+            //UpdateView();
+            
             transform.Find("Canvas/AddNumber").GetComponent<Button>().onClick.AddListener(() =>
             {
-                GameModel.count++;
-                UpdateView();
+                //交互逻辑
+                ViewModel.count++;
+                //表现逻辑
+                //UpdateView();
             });
             transform.Find("Canvas/SubNumber").GetComponent<Button>().onClick.AddListener(() =>
             {
-                GameModel.count--;
-                UpdateView();
+                ViewModel.count--;
+                //UpdateView();
             });
+        }
+        private void OnCountChanged(int newCount)
+        {
+            number.text = newCount.ToString();
         }
         void UpdateView()
         {
-            number.text = GameModel.count.ToString();
+            number.text = ViewModel.count.ToString();
+        }
+        private void OnDisable()
+        {
+            ViewModel.mOnEvent -= OnCountChanged;
+        }
+    }
+    public class ViewModel
+    {
+        private static int mCount;
+        public static Action<int> mOnEvent;
+        public static int count
+        {
+            get => mCount;
+            set
+            {
+                if (value != mCount)
+                {
+                    mCount = value;
+                    
+                    mOnEvent?.Invoke(value);
+                }
+            }
         }
     }
 }

6808f28c6841ac98f4654726b51539e8a2042e28
MangoKI <347289276@qq.com>
2022-01-20 20:36:47 +0800

    testPool删除

--- GameFramework/Assets/MGFramework/Test/Test.cs
+++ GameFramework/Assets/MGFramework/Test/Test.cs
@@ -2,19 +2,9 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    private TestPool p;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            p = PoolManager.Instance.GetObject<TestPool>();
-            p.Init();
-        }
 
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            p.Dead();
-        }
     }
 }
 

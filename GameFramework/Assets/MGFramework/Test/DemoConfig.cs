222d28fc98a5eb53dec02e3a93537ea428d06038
MangoKI <347289276@qq.com>
2022-01-20 22:36:35 +0800

    12.配置文件基类与配置设置

---
+++ GameFramework/Assets/MGFramework/Test/DemoConfig.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "DemoConfig",menuName = "MGUKI/Config/DemoConfig")]
+public class DemoConfig : ConfigBase
+{
+    public string Name;
+}
--- GameFramework/Assets/MGFramework/Test/Test.cs
+++ GameFramework/Assets/MGFramework/Test/Test.cs
@@ -1,7 +1,14 @@
+using System;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public ConfigSetting _configSetting;
+    private void Start()
+    {
+        Debug.Log(_configSetting.GetConfig<DemoConfig>("武器",1).Name);
+    }
+
     private void Update()
     {
 
